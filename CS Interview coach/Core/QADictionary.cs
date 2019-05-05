6edbeb5d7f7aa79e3de8fd134ecbe1c2c32d769e
XaspMe <kaspme@outlook.com>
2019-05-05 10:25:18 +0300

    Fixed bug with prev question, replace queue with stack.

--- CS Interview coach/Core/QADictionary.cs
+++ CS Interview coach/Core/QADictionary.cs
@@ -9,13 +9,20 @@ namespace CS_Interview_coach
     public static class QADictionary
     {
         /// <summary>
+        /// Variable to save the current position of the question, to add it to the stack of questions when generating the next.
+        /// </summary>
+        private static int currentPos;
+
+        /// <summary>
         /// Dictionary for managing answer and questions
         /// </summary>
         public static Dictionary<int, QAInstance> questionAndAnswers = new Dictionary<int, QAInstance>();
+
         /// <summary>
-        /// The queue to return to the previous question => BackToPreviousQuestion().
+        /// The stack to return to the previous question => BackToPreviousQuestion().
         /// </summary>
-        private static Queue<int> Previous = new Queue<int>(2);
+        public static Stack<int> Previous = new Stack<int>();
+
 
         /// <summary>
         /// Reloading dictionary after changing XML file with questions and answers. 1 step - clear curent value, 2 step - read XML
@@ -27,15 +34,17 @@ namespace CS_Interview_coach
         }
 
         /// <summary>
-        /// Randomly generating next instance and saving previous q. number. If XML is empty return null value
+        /// Randomly generating next instance and saving previous q. number. If XML is empty return null value.
         /// </summary>
         /// <returns></returns>
         public static QAInstance GenerateNextQuestion() 
         {
+            if(!(currentPos == 0))
+                Previous.Push(currentPos);
             if (QADictionary.questionAndAnswers.Count > 0)
             {
                 int random = new Random().Next(0, QADictionary.questionAndAnswers.Count);
-                Previous.Enqueue(random);
+                currentPos = random;
                 return new QAInstance(questionAndAnswers[random].Question, questionAndAnswers[random].Answer);
             }
             else
@@ -43,14 +52,14 @@ namespace CS_Interview_coach
         }
 
         /// <summary>
-        /// Just function for returning to previuos question. Return null if Dictionary not stored selected key
+        /// Function for returning to previuos question. Return null if stack count 0.
         /// </summary>
         /// <returns></returns>
         public static QAInstance BackToPreviousQuestion()  
         {
-            if (questionAndAnswers.ContainsKey(Previous.Peek()))
-                return questionAndAnswers[Previous.Peek()];
-            else
+            if (!(Previous.Count == 0))
+                return questionAndAnswers[Previous.Pop()];
+            else 
                 return null;
         }
     }
